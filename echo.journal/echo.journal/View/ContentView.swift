f7acde2f8d3dd6a4ba13f868660814a37d7fe570
rob-builds-future <robin-bettinghausen@gmx.de>
2025-01-09 10:25:07 +0100

    no message

---
+++ echo.journal/echo.journal/View/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  echo.journal
+//
+//  Created by Robin Bettinghausen on 09.01.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ echo.journal/echo.journal/echo_journalApp.swift
@@ -0,0 +1,17 @@
+//
+//  echo_journalApp.swift
+//  echo.journal
+//
+//  Created by Robin Bettinghausen on 09.01.25.
+//
+
+import SwiftUI
+
+@main
+struct echo_journalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
